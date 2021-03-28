376bcfc3983343b83366277bb878229e30baf2c4
Viktor Markovec <markvik42@gmail.com>
2021-03-28 15:50:02 +0300

    feat(scripts): Update build_kb.sh for build ims.ostis.kb from ims.ostis.kb_copy

--- scripts/build_kb.sh
+++ scripts/build_kb.sh
@@ -5,6 +5,8 @@ if [ ! -d "../kb.bin" ]; then
     mkdir ../kb.bin
 fi
 
+cd ../ims.ostis.kb
+python3 prepare_kb.py
 cd ..
 sc-machine/bin/sc-builder -f -c -i repo.path -o kb.bin -s config/sc-web.ini -e sc-machine/bin/extensions
 
